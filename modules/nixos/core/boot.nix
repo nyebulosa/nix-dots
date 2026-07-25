5a541215e7bf7d1ddcb40fa6eba94bbbbfbf85fc
leonillont <leonillo@proton.me>
2026-07-25 16:57:08 +0200

    feat: tpm2 unlocking

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -18,11 +18,26 @@
       efi.canTouchEfiVariables = true;
       timeout = 3;
     };
-    initrd.systemd.enable = lib.mkDefault true;
+    initrd = {
+      systemd = {
+        enable = lib.mkDefault true;
+        tpm2.enable = true;
+      };
+      availableKernelModules = [
+        "tpm_crb"
+        "tpm_tis"
+      ];
+    };
     kernelPackages = lib.mkDefault pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3; # All my current machines are v3, can be changed per host
     tmp = {
       useTmpfs = true;
       #cleanOnBoot = true; # If not tmps then use this
     };
+  };
+
+  environment.systemPackages = with pkgs; [ tpm2-tss ];
+
+  security.tpm2 = {
+    enable = true;
   };
 }
